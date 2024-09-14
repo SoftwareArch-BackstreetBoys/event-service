ffb57ccb69cfaf2013935e560b85396fb5396cf0
Awat Singdamrong <a.lubing@yahoo.com>
2024-09-15 02:57:18 +0700

    fix: client func in main

--- client/main.go
+++ client/main.go
@@ -22,12 +22,12 @@ func main() {
 	eventService := services.NewEventService(eventClient)
 
 	// err = eventService.CreateEvent("Test3", "this is the second test", "when", "where", 99, 1, "club_id string", "ping")
-	// err = eventService.GetEvent("66e5df38d30fd5c1bf9fb829")
+	err = eventService.GetEvent("66e5df38d30fd5c1bf9fb829")
 	// err = eventService.DeleteEvent("66e5df38d30fd5c1bf9fb829")
 	// err = eventService.UpdateEvent("66e5dff1d30fd5c1bf9fb82a", "Test2", "this is the first test, no it is updated!", "when", "where", 99, 1, "club_id string")
 	// err = eventService.ListEvents()
 	// err = eventService.JoinEvent("66e5dff1d30fd5c1bf9fb82a", "65e4dff1d20fd5c1bf0fb55a")
-	err = eventService.LeaveEvent("66e5dff1d30fd5c1bf9fb82a", "65e4dff1d20fd5c1bf0fb55a")
+	// err = eventService.LeaveEvent("66e5dff1d30fd5c1bf9fb82a", "65e4dff1d20fd5c1bf0fb55a")
 	if err != nil {
 		log.Fatal(err)
 	}
