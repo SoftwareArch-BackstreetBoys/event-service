599b4d1fd7a2d7659d1e526f2abac5d4665d5c8d
Fei-7 <supachokang@gmail.com>
2024-11-01 16:10:49 +0700

    feat: add GetUserIdFromJWT function

---
+++ client/util/get_user_id_from_jwt.go
@@ -0,0 +1,46 @@
+package util
+
+import (
+	"errors"
+	"log"
+	"os"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/joho/godotenv"
+)
+
+type UserClaims struct {
+	jwt.StandardClaims
+
+	Id string `json:"id"`
+}
+
+var JWT_SECRET string
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	JWT_SECRET = os.Getenv("JWT_SECRET")
+}
+
+func GetUserIdFromJWT(jwtToken string) (string, error) {
+	parsedToken, err := jwt.ParseWithClaims(jwtToken, &UserClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(JWT_SECRET), nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	if !parsedToken.Valid {
+		// comment this "if" if you want to test with expired token
+		return "", errors.New("invalid token")
+	}
+
+	userClaims := parsedToken.Claims.(*UserClaims)
+
+	return userClaims.Id, nil
+}
