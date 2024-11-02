6397c558bce0bb33f19b19e4724d53ed32b86caf
Fei-7 <supachokang@gmail.com>
2024-11-02 13:04:36 +0700

    fix: [server] fix EventParticipation model. change field type of UserId from primitive.ObjectID to string.

--- server/models/mongo_event_participation_model.go
+++ server/models/mongo_event_participation_model.go
@@ -5,5 +5,5 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 // EventParticipation represents a user's participation in an event
 type MongoEventParticipation struct {
 	EventId primitive.ObjectID `bson:"event_id,omitempty"` // MongoDB ObjectID
-	UserId  primitive.ObjectID `bson:"user_id,omitempty"`  // MongoDB ObjectID
+	UserId  string             `bson:"user_id,omitempty"`  // MongoDB ObjectID
 }
